af257ee8133d5af6978777f35b927cf669a88e04
StivenSARG <saroblesg@uqvirtual.edu.co>
2026-07-29 16:09:14 -0500

    Se crea Repositorio proyecto api mesa de ayuda

---
+++ src/main/java/com/example/API/de/Mesa/de/Ayuda/ApiDeMesaDeAyudaApplication.java
@@ -0,0 +1,13 @@
+package com.example.API.de.Mesa.de.Ayuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeMesaDeAyudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeMesaDeAyudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/API/de/Mesa/de/Ayuda/ApiDeMesaDeAyudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API.de.Mesa.de.Ayuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeMesaDeAyudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
